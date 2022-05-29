c191bb4cb0f350a6f7f7712ffd9f56e59fb3ff01
pawan-dot <71133473+pawan-dot@users.noreply.github.com>
2022-05-29 23:54:45 +0530

    base url

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import store from './store'
 import axios from 'axios'
 
 const setupAxios = () => {
-  // axios.defaults.baseURL = 'https://dating-api-server.herokuapp.com';
-  axios.defaults.baseURL = 'http://localhost:5000'
+  axios.defaults.baseURL = 'https://dating-api-server.herokuapp.com';
+  //axios.defaults.baseURL = 'http://localhost:5000'
   axios.defaults.headers = {
     'Cache-Control': 'no-cache,no-store',
     'Pragma': 'no-cache',
