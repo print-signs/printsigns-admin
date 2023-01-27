1db9ed6acad1d7f739a78f7c1a5660f4f0bd5500
pawan-dot <71133473+pawan-dot@users.noreply.github.com>
2023-01-27 14:05:48 +0530

    product e

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import store from './store'
 import axios from 'axios'
 
 const setupAxios = () => {
-  //axios.defaults.baseURL = 'https://atpapi.checkapp.one'
-  axios.defaults.baseURL = 'http://localhost:5000'
+  axios.defaults.baseURL = 'https://atpapi.checkapp.one'
+  // axios.defaults.baseURL = 'http://localhost:5000'
   axios.defaults.headers = {
     'Cache-Control': 'no-cache,no-store',
     'Pragma': 'no-cache',
